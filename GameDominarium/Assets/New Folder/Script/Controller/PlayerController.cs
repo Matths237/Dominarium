514c5fefa02ad1047a48d4416feda000004e9e87
Matths237 <matthieu.afonso@edu.devinci.fr>
2025-03-21 23:40:59 +0100

    g

--- GameDominarium/Assets/New Folder/Script/Controller/PlayerController.cs
+++ GameDominarium/Assets/New Folder/Script/Controller/PlayerController.cs
@@ -39,9 +39,22 @@ public class PlayerController : MonoBehaviour
     [Header("COLOR")]
     [SerializeField] private Color _sprintColor = Color.red;  // Couleur lors du sprint
     [SerializeField] private Color _normalColor = Color.white; // Couleur normale
+    [SerializeField] private Color _dashColor = Color.blue;  // Couleur lors du dash
+
+    [Header("DASH")]
+    [SerializeField] private float _dashSpeed = 20f;
+    [SerializeField] private float _dashDuration = 0.2f;
+    [SerializeField] private float _dashCooldown = 1f;
+    [SerializeField] private KeyCode _dashKey = KeyCode.LeftControl; // Nouvelle variable pour la touche de dash
+    private float _dashTimer;
+    private float _dashCooldownTimer;
+    private bool _isDashing;
+    private Vector2 _dashDirection;
+    private Color _initialColor; // Stocke la couleur de départ
 
     private Rigidbody2D _myRgbd2D;
     private SpriteRenderer _spriteRend;
+    private bool _isSprinting;
 
     void Awake()
     {
@@ -50,46 +63,15 @@ public class PlayerController : MonoBehaviour
 
         // Assurez-vous que la couleur de départ est la couleur normale.
         _spriteRend.color = _normalColor;
+        _initialColor = _normalColor; // Stocker la couleur de départ
     }
 
     void Update()
     {
         Move();
         WallSlide();
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (_isWallSliding && (_infiniteWallJumps || _currentWallJumps < _maxWallJumps))
-            {
-                if (_timeToStopStick <= 0)
-                {
-                    StartWallJump();
-                }
-            }
-            else if (_currentJump < _limitJump)
-            {
-                StartJump();
-                _hasJumpedSinceGrounded = true;
-            }
-        }
-
-        if (Input.GetKey(KeyCode.Space) && _isJumping)
-        {
-            ContinueJump();
-        }
-
-        if (Input.GetKey(KeyCode.Space) && _isWallJumping)
-        {
-            ContinueWallJump();
-        }
-
-
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            EndJump();
-            EndWallJump();
-        }
-
+        HandleJumpInput();
+        HandleDashInput();
 
         if (_timeSinceLastWallJump > 0)
         {
@@ -106,6 +88,17 @@ public class PlayerController : MonoBehaviour
             _currentJump++;
             _hasJumpedSinceGrounded = true;
         }
+
+        if (_dashCooldownTimer > 0)
+        {
+            _dashCooldownTimer -= Time.deltaTime;
+        }
+
+        // Maintient la couleur du dash si on est en train de dasher
+        if (_isDashing)
+        {
+            _spriteRend.color = _dashColor;
+        }
     }
 
     void FixedUpdate()
@@ -119,13 +112,22 @@ public class PlayerController : MonoBehaviour
             _myRgbd2D.linearVelocity += Vector2.up * Physics2D.gravity.y * (2f - 1) * Time.deltaTime;
         }
 
+        if (_isDashing)
+        {
+            _myRgbd2D.linearVelocity = _dashDirection * _dashSpeed;
+            _dashTimer -= Time.deltaTime;
+            if (_dashTimer <= 0)
+            {
+                EndDash();
+            }
+        }
     }
 
     void Move()
     {
         float moveInput = Input.GetAxisRaw("Horizontal");
-        bool isRunning = Input.GetKey(_runKey);
-        float targetSpeed = moveInput * _speedMove * (isRunning ? _runSpeedMultiplier : 1);
+        _isSprinting = Input.GetKey(_runKey);
+        float targetSpeed = moveInput * _speedMove * (_isSprinting ? _runSpeedMultiplier : 1);
         float currentSpeed = _myRgbd2D.linearVelocity.x;
         float accelerationRate = Mathf.Abs(moveInput) > 0.01f ? 50 : 50;
         float newSpeed = Mathf.MoveTowards(currentSpeed, targetSpeed, accelerationRate * Time.deltaTime);
@@ -138,16 +140,21 @@ public class PlayerController : MonoBehaviour
             newSpeed *= 0.7f;
         }
 
-        _myRgbd2D.linearVelocity = new Vector2(newSpeed, _myRgbd2D.linearVelocity.y);
+        if (!_isDashing) // Ne pas affecter le mouvement pendant le dash
+        {
+            _myRgbd2D.linearVelocity = new Vector2(newSpeed, _myRgbd2D.linearVelocity.y);
+        }
 
         // Gérer le changement de couleur lors du sprint
-        if (isRunning)
+        if (_isSprinting)
         {
             _spriteRend.color = _sprintColor;
+            _initialColor = _sprintColor;  // Mettre à jour _initialColor si le joueur sprint
         }
         else
         {
             _spriteRend.color = _normalColor;
+            _initialColor = _normalColor; // Mettre à jour _initialColor si le joueur ne sprint pas
         }
     }
 
@@ -266,6 +273,89 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
             _isGrounded = false;
+        }
+    }
+
+    void HandleJumpInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_isWallSliding && (_infiniteWallJumps || _currentWallJumps < _maxWallJumps))
+            {
+                if (_timeToStopStick <= 0)
+                {
+                    StartWallJump();
+                }
+            }
+            else if (_currentJump < _limitJump)
+            {
+                StartJump();
+                _hasJumpedSinceGrounded = true;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Space) && _isJumping)
+        {
+            ContinueJump();
+        }
+
+        if (Input.GetKey(KeyCode.Space) && _isWallJumping)
+        {
+            ContinueWallJump();
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            EndJump();
+            EndWallJump();
+        }
+    }
+
+    void HandleDashInput()
+    {
+        if (Input.GetKeyDown(_dashKey) && _dashCooldownTimer <= 0) // Utilisation de _dashKey
+        {
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            float verticalInput = Input.GetAxisRaw("Vertical");
+
+            if (horizontalInput == 0 && verticalInput == 0)
+            {
+                // Si aucune direction n'est donnée, dasher dans la direction du regard
+                _dashDirection = _spriteRend.flipX ? Vector2.left : Vector2.right;
+            }
+            else
+            {
+                _dashDirection = new Vector2(horizontalInput, verticalInput).normalized;
+            }
+
+            StartDash(_dashDirection);
+        }
+    }
+
+    void StartDash(Vector2 direction)
+    {
+        _isDashing = true;
+        _dashTimer = _dashDuration;
+        _dashDirection = direction;
+        _dashCooldownTimer = _dashCooldown;
+        _initialColor = _spriteRend.color;  // Stocke la couleur actuelle avant de dasher
+        _spriteRend.color = _dashColor; // Changer la couleur au début du dash
+    }
+
+    void EndDash()
+    {
+        _isDashing = false;
+        _myRgbd2D.linearVelocity = Vector2.zero; // Arrêter le dash instantanément
+
+        // Restaurer la couleur correcte à la fin du dash
+        if (_isSprinting)
+        {
+            _spriteRend.color = _sprintColor; // Restaurer la couleur de sprint si le joueur sprint toujours
+        }
+        else
+        {
+            _spriteRend.color = _normalColor; // Sinon, restaurer la couleur normale
         }
     }
 }
