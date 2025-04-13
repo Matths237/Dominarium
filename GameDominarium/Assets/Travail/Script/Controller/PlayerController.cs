b2d266804a03f1d7b5f05b9cc29c21ee1bd0bdd3
Matths237 <matthieu.afonso@edu.devinci.fr>
2025-04-13 17:14:01 +0200

    a
    a

--- GameDominarium/Assets/Travail/Script/Controller/PlayerController.cs
+++ GameDominarium/Assets/Travail/Script/Controller/PlayerController.cs
@@ -1,7 +1,19 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("HEALTH")]
+    [SerializeField] private int maxHealth = 100;
+    [SerializeField] private float invincibilityDuration = 1f;
+    [SerializeField] private float respawnDelay = 2f;
+    [SerializeField] private float damageFlashDuration = 0.1f;
+    [SerializeField] private int currentHealth;
+    private bool isInvincible = false;
+    private float invincibilityTimer;
+    private Vector3 respawnPoint;
+    private bool isDead = false;
+
     [Header("MOVE")]
     [SerializeField] private float _speedMove = 8;
     [SerializeField] private float _runSpeedMultiplier = 1.5f;
@@ -41,39 +53,65 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _dashDuration = 0.2f;
     [SerializeField] private float _dashCooldown = 1f;
     [SerializeField] private KeyCode _dashKey = KeyCode.LeftControl;
-    [SerializeField] private float _dashEndMomentumMultiplier = 0.4f; 
+    [SerializeField] private float _dashEndMomentumMultiplier = 0.4f;
     private float _dashTimer;
     private float _dashCooldownTimer;
     private bool _isDashing;
     private Vector2 _dashDirection;
-    private Color _initialColor;
 
     private Rigidbody2D _myRgbd2D;
     private SpriteRenderer _spriteRend;
+    private Collider2D _collider;
     private bool _isSprinting;
 
     [Header("COLOR")]
     [SerializeField] private Color _sprintColor = Color.red;
     [SerializeField] private Color _normalColor = Color.white;
     [SerializeField] private Color _dashColor = Color.blue;
+    [SerializeField] private Color damageColor = Color.magenta;
+    [SerializeField] private Color _deathColor = Color.black;
+
 
     void Awake()
     {
         _myRgbd2D = GetComponent<Rigidbody2D>();
         _spriteRend = GetComponent<SpriteRenderer>();
+        _collider = GetComponent<Collider2D>();
 
+        currentHealth = maxHealth;
+        respawnPoint = transform.position;
         _spriteRend.color = _normalColor;
-        _initialColor = _normalColor;
     }
 
     void Update()
+    {
+        if (isDead) return;
+
+        HandleInput();
+        UpdateTimers();
+        UpdateGroundingState();
+        UpdateColor();
+    }
+
+    void FixedUpdate()
+    {
+        if (isDead) return;
+
+        ApplyGravityModifiers();
+        HandleDashingMovement();
+    }
+
+    void HandleInput()
     {
         Move();
         WallSlide();
         HandleJumpInput();
         HandleDashInput();
+    }
 
-        if (_timeSinceLastWallJump > 0)
+    void UpdateTimers()
+    {
+         if (_timeSinceLastWallJump > 0)
         {
             _timeSinceLastWallJump -= Time.deltaTime;
         }
@@ -83,44 +121,76 @@ public class PlayerController : MonoBehaviour
             _timeToStopStick -= Time.deltaTime;
         }
 
-        if (_isGrounded && !_hasJumpedSinceGrounded && _myRgbd2D.linearVelocity.y < -0.1f)
-        {
-            _currentJump++;
-            _hasJumpedSinceGrounded = true;
-        }
-
         if (_dashCooldownTimer > 0)
         {
             _dashCooldownTimer -= Time.deltaTime;
         }
 
-        if (_isDashing)
+        if (isInvincible)
         {
-            _spriteRend.color = _dashColor;
+            invincibilityTimer -= Time.deltaTime;
+            if (invincibilityTimer <= 0)
+            {
+                isInvincible = false;
+            }
         }
     }
 
-    void FixedUpdate()
+    void UpdateGroundingState()
+    {
+         if (_isGrounded && !_hasJumpedSinceGrounded && _myRgbd2D.linearVelocity.y < -0.1f)
+        {
+            _currentJump++;
+            _hasJumpedSinceGrounded = true;
+        }
+    }
+
+    void UpdateColor()
+    {
+        if (isInvincible && !isDead)
+        {
+             // Flash logic is handled by the coroutine
+        }
+        else if (_isDashing)
+        {
+            _spriteRend.color = _dashColor;
+        }
+        else if (_isSprinting)
+        {
+             _spriteRend.color = _sprintColor;
+        }
+        else
+        {
+             _spriteRend.color = _normalColor;
+        }
+    }
+
+
+    void ApplyGravityModifiers()
     {
         if (_myRgbd2D.linearVelocity.y < 0)
         {
-            _myRgbd2D.linearVelocity += Vector2.up * Physics2D.gravity.y * (2.5f - 1) * Time.deltaTime;
+            _myRgbd2D.linearVelocity += Vector2.up * Physics2D.gravity.y * (2.5f - 1) * Time.fixedDeltaTime;
         }
         else if (_myRgbd2D.linearVelocity.y > 0 && !Input.GetKey(KeyCode.Space))
         {
-            _myRgbd2D.linearVelocity += Vector2.up * Physics2D.gravity.y * (2f - 1) * Time.deltaTime;
+            _myRgbd2D.linearVelocity += Vector2.up * Physics2D.gravity.y * (2f - 1) * Time.fixedDeltaTime;
         }
+    }
 
+    void HandleDashingMovement()
+    {
         if (_isDashing)
         {
             _myRgbd2D.linearVelocity = _dashDirection * _dashSpeed;
-            _dashTimer -= Time.deltaTime;
+            _dashTimer -= Time.fixedDeltaTime;
             if (_dashTimer <= 0)
             {
                 EndDash();
             }
         }
     }
+
 
     void Move()
     {
@@ -142,20 +212,6 @@ public class PlayerController : MonoBehaviour
         if (!_isDashing)
         {
             _myRgbd2D.linearVelocity = new Vector2(newSpeed, _myRgbd2D.linearVelocity.y);
-        }
-
-        if (!_isDashing)
-        {
-            if (_isSprinting)
-            {
-                _spriteRend.color = _sprintColor;
-                _initialColor = _sprintColor;
-            }
-            else
-            {
-                _spriteRend.color = _normalColor;
-                _initialColor = _normalColor;
-            }
         }
     }
 
@@ -192,6 +248,7 @@ public class PlayerController : MonoBehaviour
     void WallSlide()
     {
         _isWallSliding = false;
+        if (isDead) return; // Do not wallslide if dead
 
         Vector2 rightBoxPos = new Vector2(_spriteRend.bounds.max.x + distanceDW / 2, _spriteRend.bounds.center.y);
         Vector2 leftBoxPos = new Vector2(_spriteRend.bounds.min.x - distanceDW / 2, _spriteRend.bounds.center.y);
@@ -200,28 +257,24 @@ public class PlayerController : MonoBehaviour
         Collider2D rightWall = Physics2D.OverlapBox(rightBoxPos, boxSize, 0, _detectWall);
         Collider2D leftWall = Physics2D.OverlapBox(leftBoxPos, boxSize, 0, _detectWall);
 
-        if (rightWall != null)
+        if (rightWall != null && !_isGrounded)
         {
             _isWallSliding = true;
-            if (rightWall.tag != "Platform_eau")
-                _wallDirection = -1;
-            else {
-                _wallDirection = 0;
-            }
+             _wallDirection = -1;
+
         }
-        else if (leftWall != null)
+        else if (leftWall != null && !_isGrounded)
         {
             _isWallSliding = true;
-            if (leftWall.tag != "Platform_eau")
-                _wallDirection = 1;
-            else {
-                _wallDirection = 0;
-            }
+            _wallDirection = 1;
+
         }
 
         if (_isWallSliding)
         {
             _myRgbd2D.linearVelocity = new Vector2(_myRgbd2D.linearVelocity.x, Mathf.Clamp(_myRgbd2D.linearVelocity.y, -_wallSlideSpeed, float.MaxValue));
+            _currentJump = 0; // Reset jumps when wall sliding
+            _currentWallJumps = 0; // Reset wall jumps when starting slide
         }
     }
 
@@ -240,6 +293,7 @@ public class PlayerController : MonoBehaviour
         _currentJump++;
         _hasJumpedSinceGrounded = true;
         EndJump();
+        _isWallSliding = false;
     }
 
     void ContinueWallJump()
@@ -266,51 +320,26 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
-        {
-            _isGrounded = true;
-            _currentJump = 0;
-            _currentWallJumps = 0;
-            _hasJumpedSinceGrounded = false;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
-        {
-            _isGrounded = false;
-        }
-    }
-
     void HandleJumpInput()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (_isWallSliding && (_infiniteWallJumps || _currentWallJumps < _maxWallJumps))
             {
-                if (_timeToStopStick <= 0)
-                {
-                    StartWallJump();
-                }
+                StartWallJump();
             }
-            else if (_currentJump < _limitJump)
+            else if (_currentJump < _limitJump || _isGrounded) // Allow jump if grounded even if limit reached
             {
+                if (_isGrounded) _currentJump = 0; // Ensure jump count resets on ground jump
                 StartJump();
                 _hasJumpedSinceGrounded = true;
             }
         }
 
-        if (Input.GetKey(KeyCode.Space) && _isJumping)
+        if (Input.GetKey(KeyCode.Space))
         {
-            ContinueJump();
-        }
-
-        if (Input.GetKey(KeyCode.Space) && _isWallJumping)
-        {
-            ContinueWallJump();
+             if (_isJumping) ContinueJump();
+             if (_isWallJumping) ContinueWallJump();
         }
 
         if (Input.GetKeyUp(KeyCode.Space))
@@ -336,7 +365,8 @@ public class PlayerController : MonoBehaviour
                 _dashDirection = new Vector2(horizontalInput, verticalInput).normalized;
             }
 
-            StartDash(_dashDirection);
+            if (_dashDirection != Vector2.zero) // Prevent zero vector dash
+                 StartDash(_dashDirection);
         }
     }
 
@@ -346,32 +376,113 @@ public class PlayerController : MonoBehaviour
         _dashTimer = _dashDuration;
         _dashDirection = direction;
         _dashCooldownTimer = _dashCooldown;
-        _initialColor = _spriteRend.color;
         _spriteRend.color = _dashColor;
     }
 
     void EndDash()
     {
         _isDashing = false;
-        
         _myRgbd2D.linearVelocity = _dashDirection * _dashSpeed * _dashEndMomentumMultiplier;
+        UpdateColor(); // Update color immediately after dash ends
+    }
 
-        
-        if (_isSprinting)
+    public void TakeDamage(int damage)
+    {
+        if (isInvincible || isDead) return;
+
+        currentHealth -= damage;
+        isInvincible = true;
+        invincibilityTimer = invincibilityDuration;
+        StartCoroutine(DamageFlashCoroutine());
+
+        if (currentHealth <= 0)
         {
-            _spriteRend.color = _sprintColor;
-        }
-        else
-        {
-            _spriteRend.color = _normalColor;
+            currentHealth = 0;
+            Die();
         }
     }
+
+     private IEnumerator DamageFlashCoroutine()
+    {
+        _spriteRend.color = damageColor;
+        yield return new WaitForSeconds(damageFlashDuration);
+
+        // Only revert color if not dead and invincibility hasn't worn off *exactly* during the flash
+        if (!isDead && isInvincible)
+        {
+             UpdateColor(); // Revert to appropriate color based on state
+        }
+         // If invincibility ended during flash, Update will handle color next frame.
+         // If died during flash, Die() handles state.
+    }
+
+    void Die()
+    {
+        if (isDead) return;
+
+        isDead = true;
+        _myRgbd2D.linearVelocity = Vector2.zero;
+        _myRgbd2D.simulated = false; // Stop physics interactions
+        _collider.enabled = false; // Disable collisions
+        _isDashing = false; // Stop dash state
+        _isJumping = false;
+        _isWallJumping = false;
+        _isWallSliding = false;
+
+        _spriteRend.color = _deathColor; 
+
+        Invoke(nameof(Respawn), respawnDelay);
+    }
+
+    void Respawn()
+    {
+        transform.position = respawnPoint;
+        currentHealth = maxHealth;
+        isDead = false;
+        isInvincible = false; // Ensure not invincible on respawn
+        invincibilityTimer = 0;
+        _myRgbd2D.simulated = true; // Re-enable physics
+        _collider.enabled = true; // Re-enable collisions
+        _myRgbd2D.linearVelocity = Vector2.zero; // Reset velocity
+        _currentJump = 0;
+        _currentWallJumps = 0;
+        _hasJumpedSinceGrounded = false; // Will be set true on landing
+        _spriteRend.color = _normalColor; // Reset color
+        _dashCooldownTimer = 0; // Optionally reset dash cooldown
+    }
+
+
+     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Use CompareTag for efficiency if "Ground" layer is commonly checked
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            _isGrounded = true;
+            _currentJump = 0;
+            _currentWallJumps = 0;
+            _hasJumpedSinceGrounded = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            _isGrounded = false;
+        }
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Finish"))
         {
-           GameManager.Instance.StopBloc(); 
+           // GameManager.Instance.StopBloc(); // Make sure GameManager exists
+        }
+        // Example: Taking damage from a hazard trigger
+        if(collision.CompareTag("Hazard"))
+        {
+            TakeDamage(25); // Example damage amount
         }
     }
 }
